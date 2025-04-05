ad105014a36ec8d3b5c76ac055b72ea1491e7d22
Loago Moremi <loagomoremi@gmail.com>
2025-04-05 18:02:01 +0200

    Initial commit

---
+++ mobile/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCHSA570U5Uo7SRQ256Us-2PxCDdwgXWFM",
+  authDomain: "medifind-c1973.firebaseapp.com",
+  projectId: "medifind-c1973",
+  storageBucket: "medifind-c1973.firebasestorage.app",
+  messagingSenderId: "316330482055",
+  appId: "1:316330482055:web:ffcfe54cb395d243fd1b22",
+  measurementId: "G-4ZJS4TJ5XT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ web/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { auth, firestore } from './firebase';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCHSA570U5Uo7SRQ256Us-2PxCDdwgXWFM",
+  authDomain: "medifind-c1973.firebaseapp.com",
+  projectId: "medifind-c1973",
+  storageBucket: "medifind-c1973.firebasestorage.app",
+  messagingSenderId: "316330482055",
+  appId: "1:316330482055:web:ffcfe54cb395d243fd1b22",
+  measurementId: "G-4ZJS4TJ5XT"
+};
+
+const getData = async() => {
+  const data = await firestore.collection('users').get();
+  data.forEach(doc => {
+    console.log(data.docs.map(doc => doc.data()));
+  });
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
